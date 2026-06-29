97f63e7358dd5099d61639c6ce822f00f3575df7
Vikas Yadawad <vikasyadawad@gmail.com>
2026-06-29 11:50:06 +0200

    refactor: use official @vercel/otel package for next.js telemetry

--- instrumentation.js
+++ instrumentation.js
@@ -1,5 +1,7 @@
-export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
-    await import('./instrumentation.node.js');
-  }
+import { registerOTel } from '@vercel/otel';
+
+export function register() {
+  registerOTel({ 
+    serviceName: 'pollen-alerts',
+  });
 }
