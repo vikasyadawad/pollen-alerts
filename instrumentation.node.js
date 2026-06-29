96326b5b34a25937437e0f56066ec0b3cee50140
Vikas Yadawad <vikasyadawad@gmail.com>
2026-06-29 11:24:20 +0200

    fix: correct opentelemetry Resource import for newer versions

--- instrumentation.node.js
+++ instrumentation.node.js
@@ -1,7 +1,7 @@
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-http';
-import { Resource } from '@opentelemetry/resources';
+import { resourceFromAttributes } from '@opentelemetry/resources';
 import { ATTR_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
 
 // Alloy / OTel Collector HTTP endpoint.
@@ -10,7 +10,7 @@ import { ATTR_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
 const otlpEndpoint = process.env.OTEL_EXPORTER_OTLP_ENDPOINT || 'http://localhost:4318';
 
 const sdk = new NodeSDK({
-  resource: new Resource({
+  resource: resourceFromAttributes({
     [ATTR_SERVICE_NAME]: 'pollen-alerts',
   }),
   traceExporter: new OTLPTraceExporter({
